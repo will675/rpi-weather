1295959548edc94193daf5db820db80f1a7da5c9
Will <will.jenkins675@gmail.com>
2016-08-11 06:20:55 +0100

    Get temperature values

--- test/metoffice_test.py
+++ test/metoffice_test.py
@@ -1,34 +1,16 @@
-#!/usr/bin/env python
-#===============================================================================
-# weather_metoffice.py
-#
-# Get weather forecast from the Met Office and display as 8x8 icons
-#   * Met Office's doc: http://www.metoffice.gov.uk/datapoint/support/api-reference
-#   * Retrieve location id from: http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=2a26370d-c529-496c-8d9d-7c7b8468e379
-#   * Uses 'UK daily site specific forecast'
-#   * Need to have an API key from Met Office: https://register.metoffice.gov.uk/WaveRegistrationClient/public/register.do?service=datapoint
-#
-# 2016-08-07
-# Created by: Carter Nelson
-# Modified to Met Office by: Will Jenkins
-#===============================================================================
-
 import time
 import httplib
 import sys
 import json
 import ConfigParser
 
-from rpi_weather import RpiWeather
-from led8x8icons import LED8x8ICONS
-
-display = RpiWeather()
 
 METOFFICE_URL    = "datapoint.metoffice.gov.uk"
 REQ_BASE    = r"/public/data/val/wxfcs/all/json/"
-CONFIG_FILE = "weather.cfg"
+CONFIG_FILE = "../weather.cfg"
 API_KEY = None
 LOCATION_ID = None
+
 
 ICON_MAP = { # Day forecast codes only
 #   Met Office weather code         LED 8x8 icon
@@ -67,11 +49,9 @@ ICON_MAP = { # Day forecast codes only
 
 def giveup():
     """Action to take if anything bad happens."""
-    for matrix in xrange(4):
-        display.set_raw64(LED8x8ICONS['UNKNOWN'],matrix)
     print "Error occured."
     sys.exit(1)
-    
+
 def read_config(filename):
     config = ConfigParser.RawConfigParser()
     global API_KEY, LOCATION_ID
@@ -82,7 +62,7 @@ def read_config(filename):
     except Exception as err:
         print err
         giveup()
-        
+
 def make_metoffice_request():
     """Make request to metoffice.gov.uk and return data."""
     REQUEST = REQ_BASE + format(LOCATION_ID) + "?res=daily&key=" + API_KEY
@@ -99,7 +79,7 @@ def make_metoffice_request():
     
 def get_forecast():
     """Return a list of forecast results."""
-    json_data = json.loads(make_metoffice_request()) # Day & night weather forecast for 5 days (including today)
+    json_data = json.loads(make_metoffice_request())
     forecast = []
     temperature = []
     current_hour = time.localtime().tm_hour
@@ -109,26 +89,41 @@ def get_forecast():
             temperature.append(json_data["SiteRep"]["DV"]["Location"]["Period"][day]["Rep"][0]["Dm"])
     else: # Use night forecast for first day if current time is equal or after 18:00
         forecast.append(json_data["SiteRep"]["DV"]["Location"]["Period"][0]["Rep"][1]["W"])
-        temperature.append(json_data["SiteRep"]["DV"]["Location"]["Period"][day]["Rep"][1]["Nm"])
-        
+        temperature.append(json_data["SiteRep"]["DV"]["Location"]["Period"][0]["Rep"][1]["Nm"])
         for day in xrange(1, 4):
             forecast.append(json_data["SiteRep"]["DV"]["Location"]["Period"][day]["Rep"][0]["W"])
             temperature.append(json_data["SiteRep"]["DV"]["Location"]["Period"][day]["Rep"][0]["Dm"])
-            
-    return forecast
+    return forecast, temperature
     
-def print_forecast(forecast=None):
+def print_forecast(forecast=None, temperature=None):
     """Print forecast to screen."""
-    if forecast == None:
+    if (forecast == None or temperature == None):
         return
     print '-'*20
     print time.strftime('%Y/%m/%d %H:%M:%S')
     print "Location id: {0}".format(LOCATION_ID)
     print '-'*20
+    count = 0
     for daily in forecast:
         try:
             print "Daily code:", daily
             print "Icon: {0}".format(ICON_MAP[int(daily)])
+            print "Temperature: {0}".format(temperature[count])
+            count += 1
+        except Exception as err:
+            print "Unknown code: {0}".format(err)
+
+def print_temperature(temperature=None):
+    """Print temperature to screen."""
+    if temperature == None:
+        return
+    print '-'*20
+    print time.strftime('%Y/%m/%d %H:%M:%S')
+    print "Location id: {0}".format(LOCATION_ID)
+    print '-'*20
+    for daily in temperature:
+        try:
+            print "Temperature: {0}".format(temperature[daily])
         except Exception as err:
             print "Unknown code: {0}".format(err)
 
@@ -139,15 +134,18 @@ def display_forecast(forecast=None):
     for matrix in xrange(4):
         try:
             icon = ICON_MAP[int(forecast[matrix])]
-            display.set_raw64(LED8x8ICONS[icon], matrix)
+            print "icon:", icon
+            # display.set_raw64(LED8x8ICONS[icon], matrix)
         except:
-            display.set_raw64(LED8x8ICONS["UNKNOWN"], matrix)
+            print "UNKNOWN FORECAST CODE FOUND"
+            # display.set_raw64(LED8x8ICONS["UNKNOWN"], matrix)
+
 
 #-------------------------------------------------------------------------------
 #  M A I N
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
     read_config(CONFIG_FILE)
-    forecast = get_forecast()
-    print_forecast(forecast)
-    display_forecast(forecast)
+    forecast, temperature = get_forecast()
+    print_forecast(forecast, temperature)
+    # display_forecast(forecast)
