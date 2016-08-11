eeff1c7f252364a2bcf04ff565287dce45b18ab2
Will <will.jenkins675@gmail.com>
2016-08-11 06:52:04 +0100

    Add temp display to pi

--- test/metoffice_test.py
+++ test/metoffice_test.py
@@ -47,6 +47,15 @@ ICON_MAP = { # Day forecast codes only
     30:                             "STORM"     # Thunder 
 }
 
+class Unbuffered(object): # Used to ensure sleep function works as expected (http://stackoverflow.com/questions/107705/disable-output-buffering)
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
 def giveup():
     """Action to take if anything bad happens."""
     print "Error occured."
@@ -95,7 +104,7 @@ def get_forecast():
             temperature.append(json_data["SiteRep"]["DV"]["Location"]["Period"][day]["Rep"][0]["Dm"])
     return forecast, temperature
     
-def print_forecast(forecast=None, temperature=None):
+def print_forecast(forecast = None, temperature = None):
     """Print forecast to screen."""
     if (forecast == None or temperature == None):
         return
@@ -113,39 +122,36 @@ def print_forecast(forecast=None, temperature=None):
         except Exception as err:
             print "Unknown code: {0}".format(err)
 
-def print_temperature(temperature=None):
-    """Print temperature to screen."""
-    if temperature == None:
-        return
-    print '-'*20
-    print time.strftime('%Y/%m/%d %H:%M:%S')
-    print "Location id: {0}".format(LOCATION_ID)
-    print '-'*20
-    for daily in temperature:
-        try:
-            print "Temperature: {0}".format(temperature[daily])
-        except Exception as err:
-            print "Unknown code: {0}".format(err)
-
-def display_forecast(forecast=None):
+def display_forecast(forecast = None, temperature = None):
     """Display forecast as icons on LED 8x8 matrices."""
-    if forecast == None:
+    if (forecast == None or temperature == None):
         return
     for matrix in xrange(4):
         try:
             icon = ICON_MAP[int(forecast[matrix])]
-            print "icon:", icon
-            # display.set_raw64(LED8x8ICONS[icon], matrix)
+            # print "icon:", icon
+            display.set_raw64(LED8x8ICONS[icon], matrix)
         except:
-            print "UNKNOWN FORECAST CODE FOUND"
-            # display.set_raw64(LED8x8ICONS["UNKNOWN"], matrix)
+            # print "UNKNOWN FORECAST CODE FOUND"
+            display.set_raw64(LED8x8ICONS["UNKNOWN"], matrix)
+    time.sleep(5)
+    for matrix in xrange(4):
+        try:
+            value = temperature[matrix]
+            # print "temperature:", value
+            display.set_raw64(LED8x8ICONS[value], matrix)
+        except:
+            # print "TEMPERATURE NOT FOUND"
+            display.set_raw64(LED8x8ICONS["UNKNOWN"], matrix)
 
 
 #-------------------------------------------------------------------------------
 #  M A I N
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
+    # Need to override buffered version of stdout to ensure sleep function works as expected (http://stackoverflow.com/questions/107705/disable-output-buffering)
+    sys.stdout = Unbuffered(sys.stdout)
     read_config(CONFIG_FILE)
     forecast, temperature = get_forecast()
     print_forecast(forecast, temperature)
-    # display_forecast(forecast)
+    display_forecast(forecast, temperature)
