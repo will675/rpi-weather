c0aef46072e8eafd7fcd517d7874e3aefb164357
Will <will.jenkins675@gmail.com>
2016-08-11 07:08:37 +0100

    Set temp to 1 to see display

---
+++ test/bitmap_generator.py
@@ -0,0 +1,19 @@
+
+
+BITMAP = [
+[0, 0, 0, 0, 0, 0, 0, 0,],
+[0, 0, 0, 0, 0, 0, 0, 0,],
+[0, 0, 0, 0, 0, 0, 0, 0,],
+[0, 0, 0, 0, 0, 0, 0, 0,],
+[0, 0, 0, 0, 0, 0, 0, 0,],
+[0, 0, 0, 0, 0, 0, 0, 0,],
+[0, 0, 0, 0, 0, 0, 0, 0,],
+[0, 0, 0, 0, 0, 0, 0, 0,],
+]
+value = 0
+for y,row in enumerate(BITMAP):
+    row_byte = 0
+    for x,bit in enumerate(row):
+        row_byte += bit<<x    
+    value += row_byte<<(8*y)
+print '0x'+format(value,'02x')
