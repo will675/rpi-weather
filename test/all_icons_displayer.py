cf689271ccbe3cedc74ba5ac55cfdca0df517fb8
Will <will.jenkins675@gmail.com>
2016-08-26 07:22:32 +0100

    Add script to display all icons in turn

---
+++ test/all_icons_displayer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#===============================================================================
+# all_icons_displayer.py
+# 
+# 2016-08-10
+# Test module used to display all icons stored in led8x8icons.py in turn to 8x8 
+# led matrices to check how they look
+# Need to ensure that PYTHONPATH env var includes project root so can import
+# rpi-weather module from parent dir
+#===============================================================================
+
+import time
+import sys
+
+from rpi_weather import RpiWeather
+from led8x8icons import LED8x8ICONS
+
+display = RpiWeather()
+
+class Unbuffered(object):
+    """Used to ensure sleep function works as expected
+    (http://stackoverflow.com/questions/107705/disable-output-buffering)"""
+    def __init__(self, stream):
+        self.stream = stream
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
+
+def display_icons(top=None):
+    """Display numbers up to top value on LED 8x8 matrices."""
+    display.clear_disp()
+    if top == None:
+        return
+    count = 0
+    number_of_icons = LED8x8ICONS.len()
+    print number_of_icons
+    for i in xrange(top):
+        display.clear_disp()
+        for matrix in xrange(4):
+            try:
+                print count
+                # display.set_raw64(LED8x8ICONS[str(count)], matrix)
+            except:
+                print "NUMBER NOT KNOWN"
+                # display.set_raw64(LED8x8ICONS["UNKNOWN"], matrix)
+        time.sleep(1)
+        count += 1
+        
+#-------------------------------------------------------------------------------
+#  M A I N
+#-------------------------------------------------------------------------------
+if __name__ == "__main__":
+    sys.stdout = Unbuffered(sys.stdout)
+    display_numbers(40)
