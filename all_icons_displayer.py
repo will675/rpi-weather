b253e1dc8e28c5532e27cb488fd34acf775731b2
Will <will.jenkins675@gmail.com>
2016-08-26 07:43:37 +0100

    Add dict index loop to run through all values in LED8x8ICONS

--- all_icons_displayer.py
+++ all_icons_displayer.py
@@ -28,29 +28,25 @@ class Unbuffered(object):
     def __getattr__(self, attr):
         return getattr(self.stream, attr)
 
-def display_icons(top=None):
-    """Display numbers up to top value on LED 8x8 matrices."""
+def display_all_icons():
+    """Display all icons in the led8x8icons dictionary"""
     display.clear_disp()
-    if top == None:
-        return
-    count = 0
-    number_of_icons = LED8x8ICONS.len()
+    number_of_icons = len(LED8x8ICONS)
     print number_of_icons
-    for i in xrange(top):
+    for i in xrange(number_of_icons):
         display.clear_disp()
-        for matrix in xrange(4):
+        for matrix in xrange(number_of_icons):
             try:
-                print count
-                # display.set_raw64(LED8x8ICONS[str(count)], matrix)
+                print "key: {0} - value: {1}".format(LED8x8ICONS.keys()[matrix], LED8x8ICONS.values()[matrix])
+                display.set_raw64(LED8x8ICONS[str()], matrix)
             except:
                 print "NUMBER NOT KNOWN"
-                # display.set_raw64(LED8x8ICONS["UNKNOWN"], matrix)
-        time.sleep(1)
-        count += 1
+                display.set_raw64(LED8x8ICONS["UNKNOWN"], matrix)
+        time.sleep(3)
         
 #-------------------------------------------------------------------------------
 #  M A I N
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
     sys.stdout = Unbuffered(sys.stdout)
-    display_numbers(40)
+    display_all_icons()
